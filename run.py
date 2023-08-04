3da09ded978201ffb26b64edaa7581b950ea580b
strasse34 <>
2023-08-04 10:59:21 +0000

    practice

--- run.py
+++ run.py
@@ -19,13 +19,15 @@ message = greet('reza', 'mirzaie', 40)
 print(message)
 
 
- 
+# ** functionality 
 def save_data(**user):
     print(user)
 
 
 save_data(id=1, fname='reza', lname='mirzaie', age=40)
 
+
+#distracting itmes from a list or tuple
 users = [
     ['ali', 20],
     ['moha', 40],
@@ -41,9 +43,34 @@ for user in users:
 print(name)
 print(age)
 
+
+# using map(), filter, and lambda
+name = list(map(lambda user: user[0], users))
+age = list(map(lambda user: user[1], users))
+filtered = list(filter(lambda user: user[1] < 30, users))
+print(name , age, filtered)
+# or comperhention
+name = [user[0] for user in users]
+age = [user[1] for user in users]
+filtered = [user[1] for user in users if user[1] < 30]
+
+print(name , age, filtered)
+
+
+# changing a list to dic
 users_dic = {}
 for user in users:
     key = user[0]
     value = user[1]
     users_dic[key] = value
 print(users_dic)
+
+
+print(dict(users))
+
+print(list(users_dic))
+
+print(list(users_dic.items()))
+
+for x in users_dic.items():
+    print(x)
